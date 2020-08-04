b1385c4d4f28773244ad090ad283e2b26a0ee378
Yan Yee Kwok <beverly@Yans-MacBook-Pro.local>
2020-08-03 18:21:20 -0700

    start learning c sharp

---
+++ fundamental1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fundamental1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
