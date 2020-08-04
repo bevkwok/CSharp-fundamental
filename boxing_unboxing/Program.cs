3cfd1ccf9a19e86866031503d2ac2838b56fcab1
Yan Yee Kwok <beverly@Yans-MacBook-Pro.local>
2020-08-03 21:40:43 -0700

    complete boxing/unboxing

---
+++ boxing_unboxing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> boxinglist = new List<object>();
+
+            boxinglist.Add(7);
+            boxinglist.Add(28);
+            boxinglist.Add(-1);
+            boxinglist.Add(true);
+            boxinglist.Add("chair");
+
+            int sum = 0;
+            foreach (object item in boxinglist) {
+                
+                System.Console.WriteLine(item);
+                if (item is int) {
+                    sum = sum + (int)item;
+                }
+            }
+            System.Console.WriteLine(sum);
+        }
+    }
+}
